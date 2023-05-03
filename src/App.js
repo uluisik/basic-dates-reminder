078f6c768d84bbb6e8be5ff6ca838346a137a417
ahmet <ahmet55uluisik05@gmail.com>
2023-05-03 15:58:38 +0300

    create data and project finally

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
+import Data from "./Data";
 function App() {
+  const [holidays, setHolidays] = useState(Data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>National Holidays Dates</h3>
+        {holidays.map((holiday) => {
+          return (
+            <div className="holiday" key={holiday.id}>
+              <img src={holiday.img} alt="" />
+              <div>
+                <p>{holiday.title}</p>
+                <p>{holiday.date}</p>
+              </div>
+            </div>
+          );
+        })}
+        <button onClick={() => setHolidays([])}>Clear All</button>
+      </section>
+    </main>
   );
 }
 
---
+++ src/Data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id: "1",
+    title: "Ulusal Egemenlik ve Cocuk Bayramı ",
+    img: "https://pbs.twimg.com/media/CgszXo7WwAAHkRQ?format=jpg&name=large",
+    date: "23 Nisan",
+  },
+  {
+    id: "2",
+    title: "Zafer Bayramı",
+    img: "https://cdn1.ntv.com.tr/gorsel/V26SExYIj0mBRuhP8UyVDg.jpg?width=1000&mode=both&scale=both&v=1598795342325",
+    date: "30 Ağustos",
+  },
+  {
+    id: "3",
+    title: "Atatürkü Anma Gençlik ve Spor Bayramı",
+    img: "https://tvhb.org.tr/wp-content/uploads/2019/05/b5eaf96c-15a2-401a-9fa7-a61e6c8e09c3.jpg",
+    date: "19 Mayıs",
+  },
+  {
+    id: "4",
+    title: "Cumhuriyet Bayramı",
+    img: "https://pbs.twimg.com/media/DNPg8I5WsAATFHV?format=jpg&name=4096x4096",
+    date: "29 Ekim",
+  },
+];
